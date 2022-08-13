ddda2bbe6f2c02c8b7a6c6b8c93de76b2dbdfc9b
JuanCarlosAguilarB <abjuancarlos.12@gmail.com>
2022-08-12 20:40:12 -0500

    cambio

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,16 +4,16 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div class="main-container">
-        <div class="conitainer-left"><span class="animate-charcter">Juan Carlos A</span></div>
-        <div class="container-right">
+        <div className="main-container">
+        <div className="conitainer-left"><span className="animate-charcter">Juan Carlos A</span></div>
+        <div className="container-right">
             <ul>
-            <li class="waviy"> <a target="_blank"  href="https://www.linkedin.com/in/juan-carlos-aguilar-bosiga-41b067192/">linkdin</a> </li>
-            <li class="waviy"> <a target="_blank" href="https://github.com/JuanCarlosAguilarB">github</a> </li>
-            <li class="waviy"> <a target="_blank" href="">Projects</a> </li>
+            <li className="waviy"> <a target="_blank"  href="https://www.linkedin.com/in/juan-carlos-aguilar-bosiga-41b067192/">linkdin</a> </li>
+            <li className="waviy"> <a target="_blank" href="https://github.com/JuanCarlosAguilarB">github</a> </li>
+            <li className="waviy"> <a target="_blank" href="">Projects</a> </li>
         </ul>
-        <buttton class="btn">Download CV</buttton>
-        <buttton class="btn"> Contac </buttton>
+        <buttton className="btn">Download CV</buttton>
+        <buttton className="btn"> Contac </buttton>
         </div>
     </div>
     )
