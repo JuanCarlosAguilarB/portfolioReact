a1d81b803fb36bbc826a0998bd316f73e6be6318
JuanCarlosAguilarB <abjuancarlos.12@gmail.com>
2022-08-13 19:35:57 -0500

    cambio

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -12,7 +12,9 @@ const Navbar = () => {
             <li className="waviy"> <a target="_blank" href="https://github.com/JuanCarlosAguilarB">github</a> </li>
             <li className="waviy"> <a target="_blank" href="">Projects</a> </li>
         </ul>
-        <buttton className="btn">Download CV</buttton>
+        
+
+        {/* <buttton className="btn">Download CV</buttton> */}
         <buttton className="btn"> Contac </buttton>
         </div>
     </div>
