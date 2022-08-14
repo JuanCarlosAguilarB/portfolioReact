ab5704fbb049cb3b2796971334dc8994bdd7b9d8
JuanCarlosAguilarB <abjuancarlos.12@gmail.com>
2022-08-13 19:15:03 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
+// import { Banner } from "./components/Banner";
 // import { Skills } from "./components/Skills";
 // import { Projects } from "./components/Projects";
 // import { Contact } from "./components/Contact";
@@ -10,6 +10,7 @@ import { Banner } from "./components/Banner";
 
 import Navbar from "./components/navbar"
 import Projects from './components/projects';
+import Banner from "./components/banner"
 
 function App() {
   return (
